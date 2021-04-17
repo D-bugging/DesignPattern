aac2dc778167bc64aa03ed0bb54e473cae81ec2d
D-bugging <archmelo@protonmail.com>
2021-04-17 10:39:06 -0300

    Factory Method Example

---
+++ FactoryMethod/Factories/CarTransport.cs
@@ -0,0 +1,15 @@
+﻿using FactoryMethod.Vehicles;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FactoryMethod.Factories
+{
+    class CarTransport : Transport
+    {
+        public override IVehicle CreateTransport()
+        {
+            return new Car();
+        }
+    }
+}
---
+++ FactoryMethod/Factories/Transport.cs
@@ -0,0 +1,15 @@
+﻿using FactoryMethod.Vehicles;
+
+namespace FactoryMethod.Factories
+{
+    abstract class Transport
+    {
+        public void StartTransport()
+        {
+            IVehicle vehicle = CreateTransport();
+            vehicle.StartRoute();
+        }
+
+        public abstract IVehicle CreateTransport();
+    }
+}
